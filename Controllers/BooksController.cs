01102a289960408440bd9068aea115373659c124
vivek <vivek@athenaeum.in>
2021-05-06 16:46:51 +0530

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.EFCore.Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok(new { Message = "Hello world!" });
+        }
+    }
+}
