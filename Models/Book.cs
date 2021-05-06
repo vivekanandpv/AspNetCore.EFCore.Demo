d35d56694d8e5ce727490c8e0ddd4e81da996ecc
vivek <vivek@athenaeum.in>
2021-05-06 17:08:27 +0530

    done

---
+++ Models/Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.EFCore.Demo.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Edition { get; set; }
+        public double Price { get; set; }
+        public int NPages { get; set; }
+    }
+
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class BookReview
+    {
+        public int Id { get; set; }
+        public int ScoreOn5Scale { get; set; }
+        public string ReviewTitle { get; set; }
+        public string DetailedReview { get; set; }
+    }
+}
